ebd001c97cace38f69d010472a5d5711a61c972a
phamhuuminhtam <minhtamk17qth@gmail.com>
2022-07-16 22:20:34 +0700

    update

--- caseStudy_furama/furama.sql
+++ caseStudy_furama/furama.sql
@@ -269,27 +269,27 @@ END;
 
 -- câu 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).-- 
 SELECT 
-    ma_dich_vu,
-    ten_dich_vu,
-    dien_tich,
-    chi_phi_thue,
-    ten_loai_dich_vu
-FROM
+   dich_vu.ma_dich_vu,
+    dich_vu.ten_dich_vu,
+    dich_vu.dien_tich,
+    dich_vu.chi_phi_thue,
+    loai_dich_vu.ten_loai_dich_vu
+FROM(
     (SELECT 
-        ten_dich_vu,
+			ten_dich_vu,
             hop_dong.ma_dich_vu,
             ngay_lam_hop_dong,
             dich_vu.dien_tich,
-            dich_vu.chi_phi_thue,
-            loai_dich_vu.ten_loai_dich_vu AS ten_loai_dich_vu
+            dich_vu.chi_phi_thue
     FROM
         hop_dong
     JOIN dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
-    JOIN loai_dich_vu ON dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
-    GROUP BY ten_dich_vu) AS table1
-WHERE
-    NOT (MONTH(ngay_lam_hop_dong) IN (1 , 2, 3)
-        AND YEAR(ngay_lam_hop_dong) = 2021);
+    WHERE
+     (MONTH(ngay_lam_hop_dong) IN (1 , 2, 3)
+        AND YEAR(ngay_lam_hop_dong) = 2021)) as table1
+   right join dich_vu on  table1.ten_dich_vu = dich_vu.ten_dich_vu)
+   join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+   where table1.ten_dich_vu is null;
 
 -- 7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021
 SELECT 
@@ -354,7 +354,7 @@ FROM
 --   9.	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
 
 SELECT 
-    MONTH(ngay_lam_hop_dong), COUNT(ma_hop_dong)
+    MONTH(ngay_lam_hop_dong) as thang, COUNT(ma_hop_dong) as so_luong
 FROM
     hop_dong
 WHERE
@@ -376,7 +376,7 @@ FROM
     hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
         LEFT JOIN
     dich_vu_di_kem ON dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
-GROUP BY ma_hop_dong
+GROUP BY ma_hop_dong;
 
 
   
