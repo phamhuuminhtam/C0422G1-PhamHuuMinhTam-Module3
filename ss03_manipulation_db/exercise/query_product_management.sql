d7ab522e9758080e41cee412539a45864296b62b
phamhuuminhtam <minhtamk17qth@gmail.com>
2022-07-19 09:12:57 +0700

    update

--- ss03_manipulation_db/exercise/query_product_management.sql
+++ ss03_manipulation_db/exercise/query_product_management.sql
@@ -23,7 +23,7 @@ INSERT INTO order_detail (o_id, p_id, od_qty) VALUES ('1', '1', '3'),
 ('2', '3', '3');
 
 -- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
-select o_id, c_id, o_date 
+select o_id as  ma_hoa_don , o_date as ngay_ban, o_totalprice  as gia
 from `order`; 
 
 -- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
@@ -50,14 +50,14 @@ WHERE o_id IS  NULL;
 
 -- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn. Giá bán của từng loại được tính = odQTY*pPrice)
 SELECT 
-    `order`.o_id as  ma_san_pham , o_date as ngay_ban, SUM(od_qty * p_price) AS total
+    `order`.o_id as  ma_hoa_don , o_date as ngay_ban, SUM(od_qty * p_price) AS total
 FROM
     `order`
         JOIN
     order_detail ON `order`.o_id = order_detail.o_id
         JOIN
     product ON order_detail.p_id = product.p_id
-GROUP BY ma_san_pham;
+GROUP BY ma_hoa_don;
 
 
 
